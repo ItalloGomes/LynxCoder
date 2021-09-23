e5ac0d4d64f94454604b202e3ac406bb3483ae9a
Aleff-Kelvin <aleff.stampini@alpenet.com.br>
2021-09-22 23:47:37 -0300

    subindo api looca

---
+++ API/pi-project/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import com.github.britooo.looca.api.core.Looca;
+
+public class Main {
+    public static void main(String[] args) {
+        Looca looca = new Looca();
+
+//        System.out.println(looca.getSistema()+"\n");
+//
+//        System.out.println(looca.getMemoria()+"\n");
+//
+//        System.out.println(looca.getGrupoDeDiscos().getDiscos()+"\n");
+//        System.out.println(looca.getGrupoDeDiscos().getQuantidadeDeDiscos()+"\n");
+//        System.out.println(looca.getGrupoDeDiscos().getVolumes()+"\n");
+//        System.out.println(looca.getGrupoDeDiscos().getTamanhoTotal()+"\n");
+//        System.out.println(looca.getGrupoDeDiscos().getQuantidadeDeVolumes()+"\n");
+//
+//
+//        System.out.println(looca.getProcessador()+"\n");
+//        System.out.println(looca.getGrupoDeProcessos()+"\n");
+//        System.out.println(looca.getGrupoDeServicos()+"\n");
+//        System.out.println(looca.getTemperatura()+"\n");
+
+    }
+}
