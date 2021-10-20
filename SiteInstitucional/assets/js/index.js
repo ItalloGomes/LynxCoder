dad42a6431ab6754c35ffb4a897c1586d9c515f8
Aleff Kelvin <aleff.stampini@bandtec.com.br>
2021-10-20 18:09:18 -0300

    organizando arquivos

---
+++ SiteInstitucional/assets/js/index.js
@@ -0,0 +1,7 @@
+function closedForm () {
+    boxCad.classList.add('ocult');
+}
+
+function openForm () {
+    boxCad.classList.remove('ocult');
+}
---
+++ SiteInstitucional/assets/js/sidebar.js
@@ -0,0 +1,48 @@
+let sidebar = document.querySelector(".sidebar");
+let filter = document.querySelector(".filter");
+
+let closeBtn = document.querySelector("#btn");
+let closeBtnFilter = document.querySelector("#btnFilter");
+
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", ()=> {
+
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+    
+});
+
+closeBtnFilter.addEventListener("click", () => {
+
+    filter.classList.toggle("open");
+    filterBtnChange(); 
+
+});
+
+searchBtn.addEventListener("click", ()=> { 
+
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+
+});
+
+function menuBtnChange() {
+
+    if(sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+
+}
+
+function filterBtnChange() {
+
+    if(filter.classList.contains("open")) {
+        closeBtnFilter.classList.replace("bxs-left-arrow", "bx-filter");
+    } else {
+        closeBtnFilter.classList.replace("bx-filter", "bxs-left-arrow");
+    }
+
+}
