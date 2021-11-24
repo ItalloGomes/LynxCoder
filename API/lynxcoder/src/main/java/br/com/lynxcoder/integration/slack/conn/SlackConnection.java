96357dffdb5f3ffac1d1abc41fd9d7d2c6a7b474
Aleff Kelvin <aleff.stampini@bandtec.com.br>
2021-11-24 18:58:51 -0300

    atualizando url do slack

--- API/lynxcoder/src/main/java/br/com/lynxcoder/integration/slack/conn/SlackConnection.java
+++ API/lynxcoder/src/main/java/br/com/lynxcoder/integration/slack/conn/SlackConnection.java
@@ -6,7 +6,7 @@ import java.net.http.HttpClient;
 public class SlackConnection {
 
     private HttpClient client = HttpClient.newHttpClient();
-    private String URL = "https://hooks.slack.com/services/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";
+    private String URL = "https://hooks.slack.com/services/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";
 
     public HttpClient getClient() {
         return client;
