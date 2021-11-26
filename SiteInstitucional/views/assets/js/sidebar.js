4c94c4bb6d177dc9ad359f5d180fd2d53f434aca
Itallo Gomes <itallo.gomes@alpenet.com.br>
2021-11-26 10:21:24 -0300

    arrumando gráficos de computador usuário

--- SiteInstitucional/views/assets/js/sidebar.js
+++ SiteInstitucional/views/assets/js/sidebar.js
@@ -1,13 +1,5 @@
 let sidebar = document.querySelector(".sidebar");
-<<<<<<< HEAD
-// let filter = document.querySelector(".filter");
-
 let closeBtn = document.querySelector("#btn");
-// let closeBtnFilter = document.querySelector("#btnFilter");
-
-=======
-let closeBtn = document.querySelector("#btn");
->>>>>>> 429a56735d50c770ce0cd477c555ca7fd47c9173
 let searchBtn = document.querySelector(".bx-search");
 
 closeBtn.addEventListener("click", ()=> {
@@ -17,16 +9,6 @@ closeBtn.addEventListener("click", ()=> {
     
 });
 
-<<<<<<< HEAD
-// closeBtnFilter.addEventListener("click", () => {
-
-//     filter.classList.toggle("open");
-//     filterBtnChange(); 
-
-// });
-
-=======
->>>>>>> 429a56735d50c770ce0cd477c555ca7fd47c9173
 searchBtn.addEventListener("click", ()=> { 
 
     sidebar.classList.toggle("open");
@@ -43,16 +25,3 @@ function menuBtnChange() {
     }
 
 }
-<<<<<<< HEAD
-
-// function filterBtnChange() {
-
-//     if(filter.classList.contains("open")) {
-//         closeBtnFilter.classList.replace("bxs-left-arrow", "bx-filter");
-//     } else {
-//         closeBtnFilter.classList.replace("bx-filter", "bxs-left-arrow");
-//     }
-
-// }
-=======
->>>>>>> 429a56735d50c770ce0cd477c555ca7fd47c9173
