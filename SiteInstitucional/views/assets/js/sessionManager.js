3897610df4a0ed844d0341f55a3c20faecf6b57d
Daniel Pederzini <daniel.pederzini@bandtec.com.br>
2021-11-26 18:36:58 -0300

    Bug de login do admin arrumado

--- SiteInstitucional/views/assets/js/sessionManager.js
+++ SiteInstitucional/views/assets/js/sessionManager.js
@@ -6,12 +6,18 @@ function redirect_login() {
 
 function check_authentication() {
 
-    if (userSession.login == undefined)  {
-        redirect_login();
+    if (userSession.tipoLogin != 0) {
+        if (userSession.login == undefined) {
+            redirect_login();
+        }
+    } else if (userSession.tipoLogin == 0) {
+        if (userSession.login_admin == undefined) {
+            redirect_login();
+        }
     } else {
         session_validate();
     }
-    
+
 }
 
 function logoff() {
@@ -21,19 +27,19 @@ function logoff() {
 }
 
 function session_validate() {
-    fetch(`/login/sessao/${userSession.login}`, {cache:'no-store'})
-    .then(resposta => {
-        if (resposta.ok) {
-            resposta.text().then(texto => {
-                console.log('Sessão :) ', texto);    
-            });
-        } else {
-            console.error('Sessão :.(');
-            logoff();
-        } 
-    });    
+    fetch(`/login/sessao/${userSession.login}`, { cache: 'no-store' })
+        .then(resposta => {
+            if (resposta.ok) {
+                resposta.text().then(texto => {
+                    console.log('Sessão :) ', texto);
+                });
+            } else {
+                console.error('Sessão :.(');
+                logoff();
+            }
+        });
 }
 
 function close_session() {
-    fetch(`/login/sair/${userSession.login}`, {cache:'no-store'});
+    fetch(`/login/sair/${userSession.login}`, { cache: 'no-store' });
 }
