81303e0c23fc72a71195215767c54f5715785d11
Itallo Gomes <itallo.gomes@alpenet.com.br>
2021-11-28 20:37:35 -0300

    Listando usuarios na tela de equipe

--- SiteInstitucional/routes/usuarios.js
+++ SiteInstitucional/routes/usuarios.js
@@ -119,6 +119,37 @@ router.get('/usuariosSquad/:idSquad', function(req, res, next) {
   	});
 });
 
+router.get('/colaboradoresSquad/:idSquad/:idSprint', function(req, res, next) {
+	console.log('Todos os usuários de uma squad e seus pontos na sprint');
+
+    var params = {
+        squad: req.params.idSquad,
+        sprint: req.params.idSprint
+    }
+
+    var sql = `select  u.id_usuario, u.nome_usuario, 
+                (select count(*) from tb_tarefa as t where t.fk_usuario = u.id_usuario and t.fk_sprint = s.id_sprint and t.total_concluido = 100.00) as 'total concluidas',
+                (select count(*) from tb_tarefa as t where t.fk_usuario = u.id_usuario and t.fk_sprint = s.id_sprint) as 'total',
+                (select count(*) from tb_tarefa as t where t.fk_usuario = u.id_usuario and t.fk_sprint = s.id_sprint and t.total_concluido = 100.00) * 10 as 'pontuacao'
+                from tb_usuario as u
+                inner join tb_squad as sq
+                    on sq.id_squad = u.fk_squad
+                inner join tb_sprint as s
+                    on s.fk_squad = sq.id_squad
+                where sq.id_squad = ${params.squad} and s.id_sprint = ${params.sprint} and u.is_gestor = 0 order by 'pontuacao' desc`
+	
+    db.sequelizeConnection.query(sql, {
+        model: Usuario
+    }).then(resultado => {
+		
+		res.json(resultado);
+
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+  	});
+});
+
 router.get('/adminEmpresa/:fk_empresa', function(req, res, next) {
 	console.log('Recuperando admin da empresa');
 	
